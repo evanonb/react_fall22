07d7d856e646f3e278d695cbfe7964447368c274
evanonb <owenby_evan@columbusstate.edu>
2022-11-05 20:32:03 +0000

    tutorial #5

--- classdemo/src/components/Header.jsx
+++ classdemo/src/components/Header.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import MainBody from "./MainBody";
 
-class Header extends React.Component{
+class Header extends Component{
     constructor(props){
         super(props)
         this.state = {
             count: 0,
-            theme : window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+            theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light",
+            name: ""
         }
     }
     render(){
         const { linkText, linkUrl } = this.props
+        const name = this.state.name
         return(
             <header className={`App-header ${this.state.theme}`}>
                 <p className={"testing-this " + this.state.theme}>
@@ -18,12 +21,17 @@ class Header extends React.Component{
                 <a className="App-link" href={linkUrl} target="_blank" rel="noopener noreferrer">
                     {linkText}
                 </a>
+                <div>
+                    <h3> Your stateful name is: <i>{name}</i></h3>
+                    <input type="text" value={name} onChange={(e) => this.setState({ name : e.target.value})}/>
+                </div>
                 <span>
                     You have clicked on the button {this.state.count} times!
                 </span>
                 <button onClick={() => this.setState({ count : this.state.count + 1 })}>
                     Increment Count
                 </button>
+                <MainBody/>
             </header>
         )
     }
